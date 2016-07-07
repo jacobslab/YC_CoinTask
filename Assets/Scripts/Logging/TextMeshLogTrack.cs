665f50702ddc792e8341fc7138c4113fcc8476ab
conovich <conovich@gmail.com>
2016-07-07 12:52:41 -0400

    Fixed text mesh log track handling of newlines for windows.

--- Assets/Scripts/Logging/TextMeshLogTrack.cs
+++ Assets/Scripts/Logging/TextMeshLogTrack.cs
@@ -45,7 +45,7 @@ public class TextMeshLogTrack : LogTrack {
 			textToLog = " "; //log a space -- makes it easier to read it during replay!
 		}
 		else {
-			textToLog = textToLog.Replace (System.Environment.NewLine, "_NEWLINE_");
+			textToLog = Regex.Replace( textToLog, "\r?\n", "_NEWLINE_"); //will replace line breaks on mac or windows with _NEWLINE_ for easier log file parsing.
 		}
 
 		subjectLog.Log (GameClock.SystemTime_Milliseconds, subjectLog.GetFrameCount(), gameObject.name + separator + "TEXT_MESH" + separator + textToLog );
