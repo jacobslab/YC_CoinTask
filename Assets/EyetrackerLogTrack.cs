fc1e2b17aca0df0060f2d92bb3db5143caa09c84
Ansh <lightnarcissus@gmail.com>
2016-10-04 14:50:25 -0400

    Eyetracker logs screen gaze, world position, gazed object and pupil diameter

---
+++ Assets/EyetrackerLogTrack.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EyetrackerLogTrack : LogTrack
+{
+
+    //currently just logs one point at a time.
+    public void LogScreenGazePoint(Vector2 position)
+    {
+        if (ExperimentSettings_CoinTask.isLogging)
+        {
+            subjectLog.Log(GameClock.SystemTime_Milliseconds, subjectLog.GetFrameCount(), "SCREEN_GAZE_POSITION" + separator + position.x + separator + position.y);
+        }
+    }
+
+    public void LogWorldGazePoint(Vector3 position)
+    {
+        if (ExperimentSettings_CoinTask.isLogging)
+        {
+            subjectLog.Log(GameClock.SystemTime_Milliseconds, subjectLog.GetFrameCount(), "WORLD_GAZE_POSITION" + separator + position.x + separator + position.y + separator + position.z);
+        }
+    }
+
+    public void LogGazeObject(GameObject gazeObject)
+    {
+        if (ExperimentSettings_CoinTask.isLogging)
+        {
+            subjectLog.Log(GameClock.SystemTime_Milliseconds, subjectLog.GetFrameCount(), "GAZE_OBJECT" + separator + gazeObject.name);
+        }
+    }
+    public void LogLeftPupilDiameter(double pupilDiameter)
+    {
+        if (ExperimentSettings_CoinTask.isLogging)
+        {
+            subjectLog.Log(GameClock.SystemTime_Milliseconds, subjectLog.GetFrameCount(), "LEFT_PUPIL_DIAMETER" + separator + pupilDiameter.ToString("F3"));
+        }
+    }
+    public void LogRightPupilDiameter(double pupilDiameter)
+    {
+        if (ExperimentSettings_CoinTask.isLogging)
+        {
+            subjectLog.Log(GameClock.SystemTime_Milliseconds, subjectLog.GetFrameCount(), "RIGHT_PUPIL_DIAMETER" + separator + pupilDiameter.ToString("F3"));
+        }
+    }
+}
--- Assets/MyTests/GazeFollower2D.cs
+++ Assets/MyTests/GazeFollower2D.cs
@@ -8,8 +8,11 @@ using iView;
 public class GazeFollower2D : MonoBehaviour {
 
 	public Canvas myCanvas;
+    private Ray ray;
+    private RaycastHit hit;
 	Transform gazeFollower;
-
+    public LayerMask mask;
+    public EyetrackerLogTrack eyetrackerLogTrack;
 	void Awake(){
 		gazeFollower = GetComponent<Transform> ();
 	}
@@ -23,9 +26,24 @@ public class GazeFollower2D : MonoBehaviour {
 	void Update () {
 		if (gazeFollower != null) {
 			Vector2 screenGazePos = SMIGazeController.Instance.GetSample ().averagedEye.gazePosInUnityScreenCoords ();
-			Debug.Log("SCREEN POS: " + screenGazePos);
-			Vector3 worldGazePos = Camera.main.ScreenToWorldPoint(new Vector3 ( screenGazePos.x, screenGazePos.y, gazeFollower.position.z));
-			Debug.Log("WORLD POS: " + worldGazePos);
+			//Debug.Log("SCREEN POS: " + screenGazePos);
+            eyetrackerLogTrack.LogScreenGazePoint(screenGazePos);
+            double leftPupilDiameter = SMIGazeController.Instance.GetSample().leftEye.pupilDiameter;
+            eyetrackerLogTrack.LogLeftPupilDiameter(leftPupilDiameter);
+            double rightPupilDiameter= SMIGazeController.Instance.GetSample().rightEye.pupilDiameter;
+            eyetrackerLogTrack.LogRightPupilDiameter(rightPupilDiameter);
+            Vector3 worldGazePos = Camera.main.ScreenToWorldPoint(new Vector3 ( screenGazePos.x, screenGazePos.y, gazeFollower.position.z));
+            eyetrackerLogTrack.LogWorldGazePoint(worldGazePos);
+			//Debug.Log("WORLD POS: " + worldGazePos);
+            ray = Camera.main.ScreenPointToRay(screenGazePos);
+
+            Debug.DrawRay(new Vector3(screenGazePos.x, screenGazePos.y, 0f), worldGazePos, Color.red);
+            if(Physics.SphereCast(ray,0.8f,out hit,100f,mask.value))
+            {
+                Debug.Log(hit.collider.gameObject.name);
+                eyetrackerLogTrack.LogGazeObject(hit.collider.gameObject);
+               // hit.collider.gameObject.GetComponent<Renderer>().material.color = Color.green;
+            }
 			//gazeFollower.position = new Vector3(screenGazePos.x, screenGazePos.y, gazeFollower.position.z);
 			//gazeFollower.position = worldGazePos;
 
