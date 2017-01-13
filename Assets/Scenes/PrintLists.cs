27c58ebbf17e6ee35dedaf15e81ecd5c4118e90f
Ansh Patel <lightnarcissus@gmail.com>
2017-01-13 15:37:39 -0500

    Building two-way plugin communication

---
+++ Assets/Scenes/PrintLists.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintLists : MonoBehaviour {
+
+	Object[] prefabs;
+	List<GameObject> objList;
+	string allNames;
+	// Use this for initialization
+	void Start () {
+		objList = new List<GameObject> ();
+
+		prefabs = Resources.LoadAll("Prefabs/Objects");
+		for (int i = 0; i < prefabs.Length; i++) {
+			objList.Add((GameObject)prefabs[i]);
+		}
+
+		for (int i = 0; i < objList.Count; i++) {
+			allNames += objList [i].gameObject.name + "\n";
+		}
+
+		System.IO.File.WriteAllText("/Users/anshpatel/Desktop/updatedNames.txt",allNames);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/SphinxTest.cs
+++ Assets/SphinxTest.cs
@@ -5,25 +5,33 @@ using System.Runtime.InteropServices;
 using System.Diagnostics;
 
 public class SphinxTest : MonoBehaviour {
-	[DllImport ("SphinxTH")]
-	private static extern IntPtr nice();
-	private static extern IntPtr SphinxRun();
+	
 
-//	private static extern int AddTwoIntegers(int a,int b);
-	//private static extern IntPtr run_sphinx();
-	[DllImport("ASimplePlugin")]
-	//private static extern float AddTwoFloats (float a,float b);
-	private static extern IntPtr Trying();
-	//private static extern IntPtr Trying();
-	//private static extern int PrintANumber ();
+	[DllImport ("ASimplePlugin")]
+	private static extern IntPtr SphinxRun(int trialNumber, int recallNumber);
+
+	[DllImport ("ASimplePlugin")]
+	private static extern int PrintANumber();
+
+	[DllImport ("ASimplePlugin")]
+	private static extern int AddTwoIntegers(int i1,int i2);
+
+	[DllImport ("ASimplePlugin")]
+	private static extern float AddTwoFloats(float f1,float f2); 
 	// Use this for initialization
 	void Start () {
-		string usbOpenFeedback = Marshal.PtrToStringAuto (SphinxRun());
+
+		string usbOpenFeedback = Marshal.PtrToStringAuto (SphinxRun(0,1));
 		UnityEngine.Debug.Log(usbOpenFeedback);
+
+		UnityEngine.Debug.Log (PrintANumber ());
+		UnityEngine.Debug.Log (AddTwoIntegers (3, 5));
+		UnityEngine.Debug.Log (AddTwoFloats (2f, 3f));
+		//int number = Marshal.ReadInt32(new IntPtr(PrintANumber ()));
 		//string response = Marshal.PtrToStringAuto (Trying());
 		//UnityEngine.Debug.Log (response);
 		//run_sphinx ();
-		//UnityEngine.Debug.Log(AddTwoFloats(3f,2f));
+		//float okay=AddTwoFloats(2f,3f);
 		//UnityEngine.Debug.Log (PrintANumber ());
 	}
 	
