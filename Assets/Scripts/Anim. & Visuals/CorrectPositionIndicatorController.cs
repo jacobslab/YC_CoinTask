d58f4bf77508995e8b6cb66fc8707a3e6707ed39
Ansh Patel <lightnarcissus@gmail.com>
2017-12-04 15:51:39 -0500

    Fixed the connecting lines bug

--- Assets/Scripts/Anim. & Visuals/CorrectPositionIndicatorController.cs
+++ Assets/Scripts/Anim. & Visuals/CorrectPositionIndicatorController.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CorrectPositionIndicatorController : MonoBehaviour {
+public class CorrectPositionIndicatorController : MonoBehaviour
+{
 	public TextMesh PointsScoredText;
 
 	public Color RightScoreColor;
@@ -9,34 +10,39 @@ public class CorrectPositionIndicatorController : MonoBehaviour {
 
 	public Renderer PointsScoredBackground;
 	public GameObject sphere;
+
 	public Color lineRendererStartColor;
 	LineRenderer toTargetLine;
 	float lineHeight = 0.3f;
 	Vector3 startPos { get { return GetStartPos(); } }
 	LineRendererLogTrack lineLogTrack;
 
-	void Awake () {
-		toTargetLine = GetComponent<LineRenderer> ();
-		lineLogTrack = GetComponent<LineRendererLogTrack> ();
+	void Awake()
+	{
+		toTargetLine = GetComponent<LineRenderer>();
+		lineLogTrack = GetComponent<LineRendererLogTrack>();
 
 		//start off with them both at the start pos.
+		Debug.Log("setting start pos as : " + startPos.ToString());
 		toTargetLine.SetPosition(0, startPos);
 		toTargetLine.SetPosition(1, startPos);
 
 		//log start pos as the beginning and end of the line.
-		lineLogTrack.LogPoint (startPos, 0);
-		lineLogTrack.LogPoint (startPos, 1);
+		lineLogTrack.LogPoint(startPos, 0);
+		lineLogTrack.LogPoint(startPos, 1);
 	}
 
-	void Start(){
+	void Start()
+	{
+		Debug.Log("start pos: " + startPos.ToString());
 
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
 
 	}
-
 	public void ChangeToRightColor()
 	{
 		sphere.GetComponent<Renderer> ().material.color = RightScoreColor;
@@ -46,42 +52,52 @@ public class CorrectPositionIndicatorController : MonoBehaviour {
 	{
 		sphere.GetComponent<Renderer> ().material.color = WrongScorecolor;
 	}
-	public void SetLineTarget(Vector3 targetPos, Color targetColor){
+	public void SetLineTarget(Vector3 targetPos, Color targetColor)
+	{
 
-		if (toTargetLine == null) {
-			toTargetLine = GetComponent<LineRenderer> ();
+		if (toTargetLine == null)
+		{
+			toTargetLine = GetComponent<LineRenderer>();
 		}
 
-		Vector3 endPos = new Vector3 (targetPos.x, transform.position.y + lineHeight, targetPos.z); //use the current object's height
+		Vector3 endPos = new Vector3(targetPos.x, transform.position.y + lineHeight, targetPos.z); //use the current object's height
+		toTargetLine.SetPosition(0, startPos);
 		toTargetLine.SetPosition(1, endPos);
 
 		//log whenever the points change/are set.
-		lineLogTrack.LogPoint (startPos, 0);
-		lineLogTrack.LogPoint (endPos, 1);
+		lineLogTrack.LogPoint(startPos, 0);
+		lineLogTrack.LogPoint(endPos, 1);
 
 
-		toTargetLine.SetColors (lineRendererStartColor, targetColor);
+		toTargetLine.SetColors(lineRendererStartColor, targetColor);
 		//log line renderer colors
-		lineLogTrack.LogColors (lineRendererStartColor, targetColor);
+		lineLogTrack.LogColors(lineRendererStartColor, targetColor);
 
 	}
 
-	Vector3 GetStartPos(){
-		return new Vector3 (transform.position.x, transform.position.y + lineHeight, transform.position.z);
+	Vector3 GetStartPos()
+	{
+		return new Vector3(transform.GetChild(3).position.x, transform.GetChild(3).position.y + lineHeight, transform.GetChild(3).position.z);
 	}
 
-	public void SetPointsText(int points){
-		if (points <= 0) {
+	public void SetPointsText(int points)
+	{
+		if (points <= 0)
+		{
 			PointsScoredText.color = WrongScorecolor;
-		} else {
+		}
+		else
+		{
 			PointsScoredText.color = RightScoreColor;
 		}
 
 		PointsScoredText.text = "";
-		if(points < 0){
+		if (points < 0)
+		{
 			PointsScoredText.text = points.ToString(); // '-' sign added automatically, as it's a negative number
 		}
-		else if (points >= 0){
+		else if (points >= 0)
+		{
 			PointsScoredText.text = "+" + points;
 		}
 		PointsScoredText.text = PointsScoredText.text;
