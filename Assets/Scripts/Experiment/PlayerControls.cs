8d4310142926b316ff716fbd110f0c3ebd0ea0d8
Ansh Patel <lightnarcissus@gmail.com>
2019-02-28 14:48:59 -0500

    Removed backwards movement and added more tables+chairs in the Room

--- Assets/Scripts/Experiment/PlayerControls.cs
+++ Assets/Scripts/Experiment/PlayerControls.cs
@@ -63,7 +63,9 @@ public class PlayerControls : MonoBehaviour{
 	{
 		//VERTICAL
 		float verticalAxisInput = Input.GetAxis ("Vertical");
-		if ( Mathf.Abs(verticalAxisInput) > 0.0f) { //EPSILON should be accounted for in Input Settings "dead zone" parameter
+
+        //only accept forward movement
+		if (verticalAxisInput > 0.0f) { //EPSILON should be accounted for in Input Settings "dead zone" parameter
 			Vector3 movementDir = movementTransform.forward;
 			float angleDifference = 0;
 			if(wasNotMoving){
