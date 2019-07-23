ccf9c385295cebb1e51938515a6c0eb287b9a4ed
Ansh Patel <lightnarcissus@gmail.com>
2019-07-23 15:40:02 -0400

    Modified RenderCheck to add WorldToViewportPoint

---
+++ Assets/RenderCheck.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderCheck : MonoBehaviour
+{
+    private Renderer m_renderer;
+    private Vector3 screenPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_renderer.isVisible)
+        {
+            screenPos = Camera.main.WorldToViewportPoint(transform.position);
+            Experiment_CoinTask.Instance.trialController.trialLogger.LogChestVisiblity(true, screenPos);
+        }
+
+        /*else
+        {
+            screenPos = Camera.main.WorldToScreenPoint(transform.position);
+            Experiment_CoinTask.Instance.trialController.trialLogger.LogChestVisiblity(false, screenPos);
+        }*/
+    }
+}
